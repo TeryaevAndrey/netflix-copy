cf8693f996296f29e3a80ebdecaddeae7df387cd
Andrey <teryaev.2004@mail.ru>
2022-11-29 23:31:33 +0300

    fix-banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -13,9 +13,9 @@ function Banner({ top }: IBanner) {
       <div className="absolute l-0 t-0 h-[95vh] -z-10">
         <Image className="h-[100%] object-cover" src={BgImg} alt="banner" />
       </div>
-      <div className="">
-          <h1>Фильмы онлайн!</h1>
-          <p>Вы любите смотреть фильмы онлайн? Стоит задержаться на Netflix!</p>
+      <div className="h-[100%] flex flex-col justify-center space-x-2 md:space-x-10">
+          <h1 className="bannerTitle">Фильмы онлайн!</h1>
+          <p className="bannerText">Вы любите смотреть фильмы онлайн? Стоит задержаться на Netflix!</p>
           <button className="bannerBtn w-[200px] mt-5">Save changes</button>
       </div>
     </div>
